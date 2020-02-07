fbae195bd36c87095ea00c80ec749469a63f7e73
xinhao <xli74@ncsu.edu>
2020-02-07 13:31:11 -0500

    ds splitter

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # Molecular DataSets (MolDS)
 > A collection of curated and standardized Molecular DataSets (MolDS) for benchmarking machine learning methods. For all datasets, we provide standardized dataset splitting.
 
--- molds/_nbdev.py
+++ molds/_nbdev.py
@@ -4,9 +4,14 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"remove_mixture": "00_MolStandardizer.ipynb",
          "MolStandardizer": "00_MolStandardizer.ipynb",
-         "DSclearner": "00_MolStandardizer.ipynb"}
+         "DSclearner": "00_MolStandardizer.ipynb",
+         "generate_scaffold": "01_DSsplitter.ipynb",
+         "scaffold_to_smiles": "01_DSsplitter.ipynb",
+         "scaffold_split": "01_DSsplitter.ipynb",
+         "random_split": "01_DSsplitter.ipynb"}
 
-modules = ["MolStandardizer.py"]
+modules = ["MolStandardizer.py",
+           "DSsplitter.py"]
 
 doc_url = "https://XinhaoLi74.github.io/molds/"
 
